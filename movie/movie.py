d167a481e10b5f1d47529070e7934ec3df130b64
Alex <alexandre.noel@imt-atlantique.net>
2025-10-03 08:07:12 +0200

    ajout méthodes movies

--- movie/movie.py
+++ movie/movie.py
@@ -14,9 +14,12 @@ movie = ObjectType('Movie')
 movies = ObjectType('[Movie]')
 query.set_field('movie_with_id', r.movie_with_id)
 query.set_field('all_movies', r.all_movies)
+query.set_field('movie_by_title', r.movie_by_title)
 
 mutation = MutationType()
 mutation.set_field('update_movie_rate', r.update_movie_rate)
+mutation.set_field('add_movie', r.add_movie)
+mutation.set_field('delete_movie', r.delete_movie)
 
 actor = ObjectType('Actor')
 movie.set_field('actors', r.resolve_actors_in_movie)
--- movie/resolvers.py
+++ movie/resolvers.py
@@ -6,6 +6,32 @@ def movie_with_id(_,info,_id):
         for movie in movies['movies']:
             if movie['id'] == _id:
                 return movie
+            
+def movie_by_title(_,info,_title):
+    with open('{}/data/movies.json'.format("."), "r") as file:
+        movies = json.load(file)["movies"]
+        for movie in movies:
+            if movie['title'] == _title:
+                return movie
+            
+def add_movie(_,info,_id,_title,_rate,_director):
+    newmovie = {
+            "id": _id,
+            "title": _title,
+            "rating": _rate,
+            "director": _director
+    }
+    with open('{}/data/movies.json'.format("."), "r") as rfile:
+        movies = json.load(rfile)
+        for movie in movies["movies"]:
+            if movie["id"] == _id:
+                return None
+        movies["movies"].append(newmovie)
+    with open('{}/data/movies.json'.format("."), "w") as wfile:
+        json.dump(movies, wfile)
+    return newmovie
+
+        
 
 def update_movie_rate(_,info,_id,_rate):
     newmovies = {}
@@ -20,6 +46,23 @@ def update_movie_rate(_,info,_id,_rate):
     with open('{}/data/movies.json'.format("."), "w") as wfile:
         json.dump(newmovies, wfile)
     return newmovie
+
+def delete_movie(_,info,_id):
+    newmovies = {}
+    delmovie = {}
+    with open('{}/data/movies.json'.format("."), "r") as rfile:
+        movies = json.load(rfile)
+        for movie in movies["movies"]:
+            if movie["id"] == _id:
+                newmovies = movies
+                newmovies["movies"].remove(movie)
+                delmovie = movie
+                break
+    if delmovie == {}:
+        return None
+    with open('{}/data/movies.json'.format("."), "w") as wfile:
+        json.dump(newmovies, wfile)
+    return delmovie
 
 def resolve_actors_in_movie(movie, info):
     with open('{}/data/actors.json'.format("."), "r") as file:
