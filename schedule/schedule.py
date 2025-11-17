731460543d6dcf4f747c56f4a224230ed6dfe330
Julien SIMON <julien.simon@etu.univ-nantes.fr>
2025-11-17 17:01:06 +0100

    Schedule grpc

--- schedule/schedule.py
+++ schedule/schedule.py
@@ -10,6 +10,42 @@ class ScheduleServicer(schedule_pb2_grpc.ScheduleServicer):
         with open('{}/data/times.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["schedule"]
 
+    def GetMoviesByDate(self, request, context):
+        moviesid = []
+        for date in self.db:
+            if date['date'] == request.date:
+                for movieid in date["movies"]:
+                    moviesid.append(schedule_pb2.MovieID(id=movieid))
+        return schedule_pb2.MoviesID(moviesid=moviesid)
+    
+    def GetDatesForMovie(self, request, context):
+        dates = []
+        for date in self.db:
+            for movie in date["movies"]:
+                if movie == request.id:
+                    dates.append(schedule_pb2.Date(date=date["date"]))
+        return schedule_pb2.Dates(dates=dates)
+    
+    def AddSchedule(self, request, context):
+        for date in self.db:
+            if date["date"] == request.date:
+                for newmovie in request.movies:
+                    date["movies"].append(newmovie)
+                    return request
+        self.db.append({"date":request.date, "movies":request.movies})
+        return request
+    
+    def DeleteSchedule(self, request, context):
+        for date in self.db:
+            if date["date"] == request.date:
+                for movie in date["movies"]:
+                    for deleting_movie in request.movies:
+                        if movie == deleting_movie:
+                            date["movies"].remove(movie)
+                            if len(date["movies"]) == 0:
+                                self.db.remove(date)
+        return request
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     schedule_pb2_grpc.add_ScheduleServicer_to_server(ScheduleServicer(), server)
